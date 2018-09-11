1e3001a627773dda63120eda6c366ee58ae9144a
julianjca <julianjca11@gmail.com>
2018-09-11 18:59:45 +0700

    adding to cart

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -19,40 +19,37 @@ const app = new Vue({
         .catch(err => {
           console.log(err);
         });
+    },
+    addToCart(id){
+      const cart = JSON.parse(localStorage.getItem('cart'));
+      let obj = {};
+      obj.id = id;
+      cart.push(obj);
+      this.cart = cart;
+      localStorage.setItem('cart',JSON.stringify(cart));
     }
   },
   data : function(){
     return{
       showModal : false,
       products : [],
-      categories : []
+      categories : [],
+      cart : []
     };
   },
   created :
     function(){
-      // axios({
-      //   method: 'get',
-      //   url: 'http://localhost:3000/products/',
-      // })
-      //   .then(data => {
-      //     console.log(data.data.data);
-      //     app.products = data.data.data;
-      //   })
-      //   .catch(err => {
-      //     console.log(err);
-      //   });
-
-        axios({
-          method: 'get',
-          url: 'http://localhost:3000/categories/',
+      axios({
+        method: 'get',
+        url: 'http://localhost:3000/categories/',
+      })
+        .then(data => {
+          console.log(data.data.data);
+          app.categories = data.data.data;
+          app.products = data.data.data[5].products;
         })
-          .then(data => {
-            console.log(data.data.data);
-            app.categories = data.data.data;
-            app.products = data.data.data[5].products;
-          })
-          .catch(err => {
-            console.log(err);
-          });
+        .catch(err => {
+          console.log(err);
+        });
     }
 });
--- client/scripts/product.js
+++ client/scripts/product.js
@@ -1,10 +1,15 @@
 Vue.component('product-comp',{
   template : `
-  <div class="product-container hvr-grow">
+  <div class="product-container hvr-grow" :id="product._id" @click="addToCart(product._id)">
     <img src="https://via.placeholder.com/200x200" alt="">
     <p>{{product.name}}</p>
     <p>Price : {{product.price}}</p>
   </div>
   `,
-  props : ['product']
+  props : ['product'],
+  methods : {
+    addToCart(id){
+      this.$emit('add-cart',id);
+    }
+  }
 });
